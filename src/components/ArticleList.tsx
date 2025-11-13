11a4b3bebc8f686f03cbcaf72c1d9d729878a95b
mathis-fournier <mathis.fournier@laplateforme.io>
2025-11-13 11:11:12 +0100

    recherche (title / categorie / filtre (like / recent)

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -4,11 +4,9 @@ import ArticleThumbnail from "./ArticleThumbnail";
 export default function ArticleList() {
   const [data, setData] = useState([{ id: 0, title: "", description: "" }]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("");
+  const [filter, setFilter] = useState("");
   const query = "http://localhost:3001/articles?q=" + searchTerm;
-
-  function handleChange(event: any) {
-    setSearchTerm(event.target.value);
-  }
 
   useEffect(() => {
     fetch(query)
@@ -21,9 +19,48 @@ export default function ArticleList() {
       .then((data) => setData(data));
   }, [searchTerm]);
 
+  function handleChange(event: any) {
+    setSearchTerm(event.target.value);
+  }
+  function handleCategory(event: any) {
+    setCategory(event.target.value);
+  }
+  function handleFilter(event: any) {
+    setFilter(event.target.value);
+  }
+
+  // FONCTION RECHERCHE (TOUT)
+  function search(category: string = "", filterby?: string) {
+    if (data) {
+      let searchres: Array<any> = data?.map((article: any) => (
+        <ArticleThumbnail
+          key={article?.id}
+          title={article?.title || "Chargement. . ."}
+          category={article?.categoryName}
+          likeCount={article.likeCount}
+          createdAt={article.createdAt}
+          image={article?.image || "https://placehold.co/150x150"}
+          content={article.content || ""}
+        />
+      ));
+      if (category !== "") {
+        searchres = searchres.filter((d) => d.props.category === category);
+      }
+      if (filterby === "liked") {
+        searchres = searchres.sort(
+          (a, b) => b.props.likeCount - a.props.likeCount,
+        );
+      }
+      console.log("search update");
+      return searchres;
+    }
+    return "";
+  }
+
   return (
     <>
       <div className="search_div">
+        {/* BARRE RECHERCHE (= TITRE / CONTENT) */}
         <input
           onChange={handleChange}
           type="text"
@@ -31,18 +68,41 @@ export default function ArticleList() {
           id="searchbar"
           placeholder="search"
         />
+
+        {/* SELECT CATEGORIES */}
+        <select
+          name="select_category"
+          className="select_category"
+          defaultValue={""}
+          onChange={handleCategory}
+        >
+          <option value={""} disabled>
+            Categories
+          </option>
+          <option value={""}>Tous</option>
+          <option value={"Angular"}>Angular</option>
+          <option value={"React"}>React</option>
+          <option value={"API"}>API</option>
+        </select>
+
+        {/* SELECT FILTRES (RECENT / LIKE / ANCIEN) */}
+        <select
+          name="filter_by"
+          className="filter_by"
+          defaultValue={""}
+          onChange={handleFilter}
+        >
+          <option value={""} disabled>
+            Trier
+          </option>
+          <option value={"asc"}>Le plus recent</option>
+          <option value={"desc"}>Le plus ancien</option>
+          <option value={"liked"}>Le plus aimé</option>
+        </select>
       </div>
-      <div className="article-list">
-        {data?.map((article: any) => (
-          <ArticleThumbnail
-            key={article?.id}
-            title={article?.title || "Chargement. . ."}
-            // category={article?.category}
-            image={article?.image || "https://placehold.co/150x150"}
-            content={article.content || ""}
-          />
-        ))}
-      </div>
+
+      {/* AFFICHAGE RESULT RECHERCHE */}
+      <div className="article-list">{search(category, filter)}</div>
     </>
   );
 }
